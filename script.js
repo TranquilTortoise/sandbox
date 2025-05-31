e9d0de12e81d7cbcaa0d287beceebdaaa35e85b9
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-05-30 21:34:22 -1000

    n/a

---
+++ script.js
@@ -0,0 +1,12 @@
+const listItem = document.querySelector("ul");
+
+const input = document.querySelector("input");
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+	const newInput = input.textContent;
+    input.textContent = "";
+
+    const newItem = document.createElement("li");
+    const new
+	});
