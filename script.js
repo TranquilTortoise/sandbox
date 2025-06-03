c90f9ca4f79540d20d0bf386e3ec97cb30eeab5e
Tranquil Tortoise <SlinkTheTranquilTortoise@gmail.com>
2025-06-03 12:42:17 -1000

    play

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const listItem = document.querySelector("ul");
+let users = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
+];
 
-const input = document.querySelector("input");
+//code
+function groupById(arr) {
+    return arr.reduce((acc, currentItem) => {
+        acc[currentItem.id] = currentItem;
+        return acc;
+    }, {});
+}
 
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
-	const newInput = input.textContent;
-    input.textContent = "";
 
-    const newItem = document.createElement("li");
-    const new
-	});
+let usersById = groupById(users);
+
+console.log(usersById);
